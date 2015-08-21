c1cd884490cb4646060007492e87db588b1d89e0
N Sankar <sankar.narayanan@citi.com>
2015-08-21 09:14:41 +0530

    Building game logic..

---
+++ JavaServletSample/src/com/sample/models/BoardState.java
@@ -0,0 +1,77 @@
+package com.sample.models;
+
+public class BoardState {
+	String first,second,third,fourth,fifth,sixth,seventh,eigth,ninth;
+
+	public String getFirst() {
+		return first;
+	}
+
+	public void setFirst(String first) {
+		this.first = first;
+	}
+
+	public String getSecond() {
+		return second;
+	}
+
+	public void setSecond(String second) {
+		this.second = second;
+	}
+
+	public String getThird() {
+		return third;
+	}
+
+	public void setThird(String third) {
+		this.third = third;
+	}
+
+	public String getFourth() {
+		return fourth;
+	}
+
+	public void setFourth(String fourth) {
+		this.fourth = fourth;
+	}
+
+	public String getFifth() {
+		return fifth;
+	}
+
+	public void setFifth(String fifth) {
+		this.fifth = fifth;
+	}
+
+	public String getSixth() {
+		return sixth;
+	}
+
+	public void setSixth(String sixth) {
+		this.sixth = sixth;
+	}
+
+	public String getSeventh() {
+		return seventh;
+	}
+
+	public void setSeventh(String seventh) {
+		this.seventh = seventh;
+	}
+
+	public String getEigth() {
+		return eigth;
+	}
+
+	public void setEigth(String eigth) {
+		this.eigth = eigth;
+	}
+
+	public String getNinth() {
+		return ninth;
+	}
+
+	public void setNinth(String ninth) {
+		this.ninth = ninth;
+	}
+}
